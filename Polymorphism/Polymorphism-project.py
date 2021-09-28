6860f7323efe8facc7e6b9171b0ca4fa43521ad5
RandyJCodes <Randalljackson20@hotmail.com>
2021-09-27 21:45:51 -0400

    Polymorphis Python Project
    Created a project that uses parent and child classes. the child classes utilize polymorphism on the parent class method.

---
+++ Polymorphism/Polymorphism-project.py
@@ -0,0 +1,51 @@
+#
+# Python: 3.9.5
+#
+# Author: Randall Jackson
+#
+#
+# Purpose: Project with a parent class and 2 child classes.
+# The Parent class utilizes a function.
+# Child classes utilize polymorphism on the parent class method. 
+# 
+
+# Parent class
+class Vehicle:
+    brand = "Unknown"
+    color = "Unknown"
+    fuel = "Water"
+    
+    def information(self):
+        msg = "\nBrand: {}\nColor: {}\nFuel: {}".format(self.brand,self.color,self.fuel)
+        return msg
+
+# child class
+class Plane(Vehicle):
+    brand = "Airbus"
+    color = "White"
+    fuel = "Hydrogen"
+
+    def flight(self):
+        msg ="\nPlanes go very fast and fly high in the sky!"
+        return msg
+    
+# another child class
+class Car(Vehicle):
+    brand = "Mazda"
+    color = "Blue"
+    fuel = "Gasoline"
+
+    def drive(self):
+        msg = "\nCars are much faster than going on foot and horseback!"
+        return msg
+
+if __name__ == "__main__":
+    plane = Plane()
+    print(plane.information())
+    print(plane.flight())
+    
+    car = Car()
+    print(car.information())
+    print(car.drive())
+
+    
